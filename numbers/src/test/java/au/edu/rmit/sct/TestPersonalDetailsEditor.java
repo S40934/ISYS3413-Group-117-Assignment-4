87e5f1eebe7b6f709712a6aae2ae74a22b0cf2a2
WTKirby <143605272+Kir7449by@users.noreply.github.com>
2025-06-06 15:26:46 +1000

    editor test file creation and moving

---
+++ numbers/src/test/java/au/edu/rmit/sct/TestPersonalDetailsEditor.java
@@ -0,0 +1,45 @@
+package au.edu.rmit.sct;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+
+class TestPersonalDetailsEditor{
+    @Test
+    void testUpdatePersonalDetails_testCase1(){
+        //TestCase 1: Check function with underaged person
+        PersonalDetailsEditor testVal = new PersonalDetailsEditor("testUnderagedPerson.txt");
+        testVal.updatePersonalDetails("56s_d%&fAB", "John", "Doe", "32|Highland Street|Melbourne|Victoria|Australia", "15-11-1990");
+        assertFalse(testVal.updatePersonalDetails("56s_d%&fB", "John", "Doe", "32|Highland Street|Melbourne|Victoria|Australia", "15-11-1990"));
+    }
+    @Test
+    void testUpdatePersonalDetails_testCase2(){
+        //TestCase 2: Check function with birthday change
+        PersonalDetailsEditor testVal = new PersonalDetailsEditor("testBirthdayPerson.txt");
+        testVal.updatePersonalDetails("56s_d%&fB", "John", "Doe", "32|Highland Street|Melbourne|Victoria|Australia", "15-11-1990");
+        assertFalse(testVal.updatePersonalDetails("56s_d%&fB", "John", "Doe", "32|Highland Street|Melbourne|Victoria|Australia", "15-11-1990"));
+    }
+    @Test
+    void testUpdatePersonalDetails_testCase3(){
+        //TestCase 3: Check function with birthday change
+        PersonalDetailsEditor testVal= new PersonalDetailsEditor("testFirstEvenPerson.txt");
+        testVal.updatePersonalDetails("56s_d%&fAB", "John", "Doe", "32|Highland Street|Melbourne|Victori|Australia", "15-11-1990");
+        assertFalse(testVal.updatePersonalDetails("56s_d%&fB", "John", "Doe", "32|Highland Street|Melbourne|Victoria|Australia", "15-11-1990"));
+    }
+    @Test
+    void testUpdatePersonalDetails_testCase4(){
+        //TestCase 4: Check function with invalid person
+        PersonalDetailsEditor testVal= new PersonalDetailsEditor("testInvalidPerson.txt");
+        testVal.updatePersonalDetails("56s_d%&fAB", "John", "Doe", "32|Highland Street|Melbourne|Victoria|USA", "15-11-1990");
+        assertFalse(testVal.updatePersonalDetails("56s_d%&fB", "John", "Doe", "32|Highland Street|Melbourne|Victoria|Australia", "15-11-1990"));
+    }
+    @Test
+    void testUpdatePersonalDetails_testCase5(){
+        //TestCase 5: Check function with invalid detail edits / function parameters
+        //check if it is format correctly
+        PersonalDetailsEditor testVal= new PersonalDetailsEditor("testWrongEditsPerson.txt");
+        testVal.updatePersonalDetails("ssssssssss", "John", "Doe", "32|Highland Street|Melbourne|Victoria|Australia", "32-11-1990");
+        assertFalse(testVal.updatePersonalDetails("ssssssssss", "John", "Doe", "32|Highland Street|Melbourne|Victoria|Australia", "15-11-1990"));
+
+    }
+}
